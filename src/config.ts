637735c36468b83a3d60c687d92569e313e957bc
David Gasquez <davidgasquez@gmail.com>
2023-10-31 12:28:31 +0000

    feat: 👔 skeleton

---
+++ src/config.ts
@@ -0,0 +1,14 @@
+export default {
+  title: `David Gasquez`,
+  description: `Personal Website and Blog`,
+  author: `David Gasquez`,
+  url: `https://davidgasquez.github.io`, // No trailing slash!
+  // JSON LD
+  name: `David Gasquez`,
+  // Facebook URL
+  facebookURL: `https://facebook.com`,
+  // Twitter URL
+  twitterURL: `https://twitter.com/jaydanurwin`,
+  // Instagram URL
+  instagramURL: `https://instagram.com/jaydanurwin`,
+};
---
+++ src/pages/brain/memo.md
@@ -0,0 +1,3 @@
+# Memo
+
+This is a test!
