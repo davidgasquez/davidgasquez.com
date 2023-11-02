17b01541d8c336895322e9eb949efcdd2a4fd47f
David Gasquez <davidgasquez@gmail.com>
2023-11-02 15:37:43 +0000

    Add rehype-autolink-headings and rehype-slug to markdown config.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,13 @@ import tailwind from "@astrojs/tailwind";
 
 import sitemap from "@astrojs/sitemap";
 
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+
 export default defineConfig({
   site: "https://davidgasquez.github.io/",
   integrations: [tailwind(), sitemap()],
+  markdown: {
+    rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, { behavior: "wrap" }]],
+  },
 });
