d1b512470b610ea57ce7069d6e7e1d0a743c2e99
David Gasquez <davidgasquez@gmail.com>
2024-07-07 06:58:46 +0000

    feat: 📝 add blog post

--- astro.config.js
+++ astro.config.js
@@ -1,16 +1,18 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
-
 import rehypeSlug from "rehype-slug";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import mdx from "@astrojs/mdx";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://davidgasquez.github.io/",
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind(), sitemap(), mdx()],
   trailingSlash: "ignore",
   markdown: {
-    rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, { behavior: "wrap" }]],
-  },
+    rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, {
+      behavior: "wrap"
+    }]]
+  }
 });
